9349c1a64373d49895de6662f9504ba9d0b04d2d
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-09-01 21:11:35 +0300

    pycodestyle

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,7 +7,6 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple:
     """index_range function"""
-    start_index = (page  - 1) * (page_size)
+    start_index = (page - 1) * (page_size)
     end_index = start_index + page_size
     return (start_index, end_index)
-    
