7255ce152ba6628391ca88a5385e1bea66176d04
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-09-10 08:59:43 +0300

    lifo caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -9,16 +9,22 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """initializes class with base init"""
         super().__init__()
+        self.lifo = []
 
     def put(self, key, item):
         """Adds item to cache"""
         if key is None or item is None:
             return
 
+        if key not in self.cache_data:
+            self.lifo.append(key)
+
+        self.cache_data[key] = item
+
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            newest_key = list(self.cache_data())[-2]
-            del self.cache_data[newest_key]
+            newest_key = self.lifo.pop(-2)
             print(f"DISCARD: {newest_key}")
+            del self.cache_data[newest_key]
 
     def get(self, key):
         """gets an item from cache using the key"""
